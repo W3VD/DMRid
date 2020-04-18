70de2ace5abbfe799037745e08a28410fc214cef
unknown <brenden@BKM.W3VD.RADIO>
2020-04-18 13:23:46 -0400

    first commit

---
+++ DMRid.ps1
@@ -0,0 +1,46 @@
+﻿# Import Functions
+. C:\DMRid\sql.ps1
+. C:\DMRid\outdatatable.ps1
+
+# SQL Variables - Use only one connection string!
+$server = '127.0.0.1'
+$database = 'DMR'
+$sqlConnection = "Data Source=$server;Integrated Security=SSPI;Initial Catalog=$database" # Integrated Windows authentication
+#$sqlConnection = "Server=$server;Database=$database;User ID=XXXX;Password=XXXX" # SQL Authetication
+
+# File Path Variables
+$downfile = 'C:\temp\user.csv'
+$outfile = 'C:\temp\UID Lookup.txt'
+$proscanfile = 'C:\ProScan\UID Lookup.txt'
+$anytonefile = 'C:\DMRid\anytoneContacts.csv'
+
+# Download new DMR database file
+Invoke-WebRequest -Uri 'https://www.radioid.net/static/user.csv' -OutFile $downfile
+
+# Import CSV file, convert to Powershell Data Table, truncate table of old data, and use SQL BulkCopy function to upload new data
+$dataImport = Import-Csv -Path $downfile | Out-DataTable
+$returnCode = executeStoredProcedure $sqlConnection 'TRUNCATE TABLE [DMR].[dbo].[user]'
+$returnCode = BulkCopy $sqlConnection '[DMR].[dbo].[user]' $dataImport
+
+# Query the ProScan SQL View and Export CSV file without double quotes, copy to ProScan dir
+selectQuery $sqlConnection 'SELECT [#SQL] FROM [dbo].[proscan]' | ConvertTo-Csv -NoTypeInformation | % {$_.Replace('"','')} | Out-File $outfile
+Copy-Item -Path $outfile -Destination $proscanfile -Force
+
+# Clean Up our mess
+Remove-Item -Path $downfile
+Remove-Item -Path $outfile
+
+# Export Anytone 878 Digital Contact List
+$anytoneSQL = 'SELECT [No.]
+                      ,[Radio ID]
+                      ,[Callsign]
+                      ,[Name]
+                      ,[City]
+                      ,[State]
+                      ,[Country]
+                      ,[Remarks]
+                      ,[Call Type]
+                      ,[Call Alert]
+                  FROM [DMR].[dbo].[anytone]'
+
+selectQuery $sqlConnection $anytoneSQL | Export-Csv -Path $anytonefile -NoTypeInformation -Force
---
+++ sql.ps1
@@ -0,0 +1,137 @@
+﻿function selectQuery($connectionString, $sql)
+{
+    # Build SQL connection
+    $connection = New-Object System.Data.SqlClient.SqlConnection($connectionString)
+    $command = $connection.CreateCommand()
+    $command.CommandText = $sql
+    $command.CommandTimeout = 0
+    $adapter = New-Object System.Data.SqlClient.SqlDataAdapter $command
+    $dataset = New-Object System.Data.DataSet
+
+    # Query SQL server
+    $connection.Open()
+    $adapter.Fill($dataset) | out-null
+    $connection.Close()
+
+    # Return Data
+    $dataset.Tables[0]
+
+}
+
+function selectQueryTable($connectionString, $sql)
+{
+    # Build SQL connection
+    $connection = New-Object System.Data.SqlClient.SqlConnection($connectionString)
+    $command = $connection.CreateCommand()
+    $command.CommandText = $sql
+    $command.CommandTimeout = 0
+    $adapter = New-Object System.Data.SqlClient.SqlDataAdapter $command
+    $dataset = New-Object System.Data.DataSet
+
+    # Query SQL server
+    $connection.Open()
+    $adapter.Fill($dataset) | out-null
+    $connection.Close()
+
+    # Return Data
+    return $dataset
+
+}
+
+function executeStoredProcedure($connection, $query)
+{
+    
+    $sqlConnection = new-object System.Data.SqlClient.SqlConnection $connection
+    $sqlConnection.Open() 
+     
+    $sqlCmd = new-object System.Data.SqlClient.SqlCommand($query, $sqlConnection)
+    $sqlCmd.CommandTimeout = 300
+ 
+    $sqlCmd.Parameters.Add("@ReturnValue", [System.Data.SqlDbType]"Int") 
+    $sqlCmd.Parameters["@ReturnValue"].Direction = [System.Data.ParameterDirection]"ReturnValue"
+ 
+    $sqlCmd.ExecuteNonQuery() | out-null
+    $sqlConnection.Close() 
+ 
+    [int]$sqlCmd.Parameters["@ReturnValue"].Value
+     
+}
+
+function getDataStoredProcedure($connection, $storedProcedure, $param1, $param1Value)
+{
+    #SQL Connection - connection to SQL server
+    $sqlConnection = new-object System.Data.SqlClient.SqlConnection
+    $sqlConnection.ConnectionString = $connection
+ 
+    #SQL Command - set up the SQL call
+    $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
+    $sqlCommand.Connection = $sqlConnection
+    $sqlCommand.CommandText = "$storedProcedure $param1"
+    $sqlCommand.Parameters.AddWithValue($param1,$param1Value) | out-null
+    $sqlCommand.Parameters.Add("@ReturnValue", [System.Data.SqlDbType]"Int") | out-null
+    $sqlCommand.Parameters["@ReturnValue"].Direction = [System.Data.ParameterDirection]"ReturnValue"
+ 
+    #Open SQL Connection
+    $sqlConnection.Open();
+    
+    #SQL Adapter - get the results using the SQL Command
+    $sqlAdapter = new-object System.Data.SqlClient.SqlDataAdapter 
+    $sqlAdapter.SelectCommand = $sqlCommand
+    $dataSet = new-object System.Data.Dataset
+    try{$recordCount = $sqlAdapter.Fill($dataSet) }
+    catch{write-host "Not able to execute getDataStoredProcedure VARIABLES; $connection $storedProcedure $param1 $param1Value"}
+ 
+    #Close SQL Connection
+    $sqlConnection.Close();
+ 
+    #Get single table from dataset
+    if([int]$sqlCommand.Parameters["@ReturnValue"].Value -eq 0){$dataSet.Tables[0]}
+    else{write-host "Stored Procedure Failed, Return Code not 0  VARIABLES; $connection $storedProcedure $param1 $param1Value"}
+}
+
+function getDataStoredProcedureTable($connection, $storedProcedure)
+{
+    #SQL Connection - connection to SQL server
+    $sqlConnection = new-object System.Data.SqlClient.SqlConnection
+    $sqlConnection.ConnectionString = $connection
+ 
+    #SQL Command - set up the SQL call
+    $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
+    $sqlCommand.Connection = $sqlConnection
+    $sqlCommand.CommandText = "$storedProcedure"
+    $sqlCommand.Parameters.Add("@ReturnValue", [System.Data.SqlDbType]"Int") | out-null
+    $sqlCommand.Parameters["@ReturnValue"].Direction = [System.Data.ParameterDirection]"ReturnValue"
+ 
+    #Open SQL Connection
+    $sqlConnection.Open();
+    
+    #SQL Adapter - get the results using the SQL Command
+    $sqlAdapter = new-object System.Data.SqlClient.SqlDataAdapter 
+    $sqlAdapter.SelectCommand = $sqlCommand
+    $dataSet = new-object System.Data.Dataset
+    try{$recordCount = $sqlAdapter.Fill($dataSet) }
+    catch{write-host "Not able to execute getDataStoredProcedure VARIABLES; $connection $storedProcedure"}
+ 
+    #Close SQL Connection
+    $sqlConnection.Close();
+ 
+    #Get single table from dataset
+    if([int]$sqlCommand.Parameters["@ReturnValue"].Value -eq 0){$dataSet}
+    else{write-host "Stored Procedure Failed, Return Code not 0  VARIABLES; $connection $storedProcedure"}
+}
+
+function BulkCopy($connectionString, $table, $data)
+{
+    # Build SQL connection
+    $connection = New-Object System.Data.SqlClient.SqlConnection($connectionString)
+    $bulkCopy = New-Object System.Data.SqlClient.SqlBulkCopy($connection)
+    $bulkCopy.DestinationTableName = $table
+
+    # Bulk Copy to SQL server
+    $connection.Open()
+    $bulkCopy.WriteToServer($data) | out-null
+    $connection.Close()
+
+    # Return
+    return 0
+}
